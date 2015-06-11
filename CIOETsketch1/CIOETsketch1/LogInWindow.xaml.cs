8f03090c732f10e9e69cd17f31c3a6c512b71246
dsuarezlogans <dsuarezlogans@gmail.com>
2015-06-11 14:44:05 -0430

    Subiendo la App COIET

---
+++ CIOETsketch1/CIOETsketch1/LogInWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace CIOETsketch1
+{
+	/// <summary>
+	/// Interaction logic for LogInWindow.xaml
+	/// </summary>
+	public partial class LogInWindow : MetroWindow
+	{
+		public LogInWindow()
+		{
+			this.InitializeComponent();
+
+
+            
+			
+			// Insert code required on object creation below this point.
+		}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   
+            string u, p;
+          
+        
+            bool uv=false, pv=false;
+            u = tbuser.Text;
+            p = tbpass.Password;
+
+            if (u.CompareTo("doctor") == 0)
+            {
+                uv = true;
+            }
+
+            if (p.CompareTo("123456") == 0)
+            {
+                pv = true;
+            }
+            if(uv==true && pv==true)
+            {
+                uv = false; pv = false;
+                MainWindow m = new MainWindow();
+                m.Show();
+                this.Close();
+
+            }
+            else
+            {
+                lblog.Content="Error: Usuario o Contraseña incorrecta";
+                tbuser.Focus();
+                tbuser.Clear();
+                tbpass.Clear();
+            
+                uv = false; pv = false;
+            }
+        }
+
+        private void tbpass_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            lblog.Content = "";
+        }
+
+        
+	}
+}
---
+++ CIOETsketch1/CIOETsketch1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace CIOETsketch1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            registroPacientesFlyout.IsOpen = true;
+        }
+
+        private void buttonAñadir_Click(object sender, RoutedEventArgs e)
+        {
+            citasFlyout.IsOpen = true;
+            
+        }
+
+        
+        private void btOrtodoncia_Click(object sender, RoutedEventArgs e)
+        {
+            OrtodonciaFlyout.IsOpen = true;
+
+        }
+
+        
+    }
+}
